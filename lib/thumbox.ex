980b23aadcb06dd1ddfc219f6fe8d222ec5c8068
Boris Kotov <bk@webdeb.de>
2018-05-02 00:00:58 +0200

    initial commit

---
+++ lib/thumbox.ex
@@ -0,0 +1,18 @@
+defmodule Thumbox do
+  @moduledoc """
+  Documentation for Thumbox.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Thumbox.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/thumbox_test.exs
@@ -0,0 +1,8 @@
+defmodule ThumboxTest do
+  use ExUnit.Case
+  doctest Thumbox
+
+  test "greets the world" do
+    assert Thumbox.hello() == :world
+  end
+end
